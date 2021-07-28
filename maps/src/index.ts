97ba9c1c4119a2391b7675df7487f31e1ded9d25
Necati Soyata <necati.soyata@gmail.com>
2021-07-28 08:26:38 +0300

    "final commit"

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -4,10 +4,8 @@ import { Company } from "./Company";
 
 const user = new User();
 
-console.log(user);
-
 const company = new Company();
 
 const customMap = new CustomMap("map");
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
